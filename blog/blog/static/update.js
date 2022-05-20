590d875bada3083a611de8cd28232d75b19319a9
Eliott <fbgus333@naver.com>
2022-05-20 19:25:21 +0900

    title.value confirm

--- blog/blog/static/update.js
+++ blog/blog/static/update.js
@@ -2,4 +2,16 @@ let inner_title = document.querySelector('.input-title')
 let inner_content = document.querySelector('.input-content')
 
 
-
+function submit_it () {
+  let form = document.querySelector('.form-box');
+  if (inner_title.value == '' || inner_content.value == '') { 
+    alert('입력하세여');
+  } else {
+    let edit_confirm = confirm('수정하시겠습니까?');
+    if (!edit_confirm) {
+      return false;
+    } else {
+      form.submit();
+    }
+  }
+}
