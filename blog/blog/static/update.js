45023e1e7ce563cbed36575bbf37ee34d011269f
Eliott <fbgus333@naver.com>
2022-05-20 18:01:56 +0900

    update method

--- blog/blog/static/update.js
+++ blog/blog/static/update.js
@@ -1,13 +1,26 @@
+let inner_title = document.querySelector('.input-title')
+let inner_content = document.querySelector('.input-content')
 
 
+function update_url (pk) {
+  location.href = `http://127.0.0.1:8000/blog/update/${pk}`
+}
 
-function move_url (pk) {
+function delete_url (pk) {
   location.href = `http://127.0.0.1:8000/blog/delete/${pk}`
 }
 
 function update_onclick (pk) {
-  if (!confirm("정말 수정하시겠습니까?")) {
-} else {
-   move_url(pk)
-}
+  if( inner_title == null ) {
+    alert('제목을 입력해주세요') 
+    } else {
+      if (!confirm("정말 수정하시겠습니까?")) {
+      } else {
+         delete_url(pk);
+      }
+    }
+  }
+
+function init(title) {
+  console.log(title.value)
 }
